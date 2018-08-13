8913a755b776b83fabd86b83ac64886c437c69fd
FriederikeHanssen <friederike.hanssen@student.uni-tuebingen.de>
2018-08-13 18:15:28 +0200

    change enumlist creation

--- src/main/java/submodule/lexica/Other.java
+++ src/main/java/submodule/lexica/Other.java
@@ -19,8 +19,7 @@ public enum Other {
     static {
         List<String> list = new ArrayList<>();
         for (Other other : Other.values()) {
-            //System.out.println("Dep " + other.toString());
-            String name = other.original;
+            String name = other.toString();
             list.add(name);
         }
         enumList = Arrays.asList(list.toArray(new String[0]));
